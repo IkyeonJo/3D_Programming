9e368c378b2c67995d4c9637f2ae138d824e8323
IkyeonJo <joiyfull@gmail.com>
2022-12-28 22:55:47 +0900

    add: threejs particle practice

---
+++ Study/web_dev_basics_review/MBTI_Test_Site/js/questions.js
@@ -0,0 +1,8 @@
+const progressValueEl = document.querySelector('.progress .value')
+const numberEl = document.querySelector('.number')
+const questionEl = document.querySelector('.question')
+const choice1El = document.querySelector('.choice1')
+const choice2El = document.querySelector('.choice2')
+
+
+
